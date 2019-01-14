d027be0cee62084adfacd005ab1b5153666d6439
coldJune <785691557@qq.com>
2019-01-14 17:20:09 +0800

    线性表链式实现

---
+++ list/src/main/java/LinkedList.java
@@ -0,0 +1,126 @@
+public class LinkedList implements List {
+    private Node head;
+    private int size;
+    LinkedList(){
+        head = new Node(null,null);
+        size = 0;
+    }
+    public void clear() {
+        for(Node p = head;p.next!=null;p=p.next){
+            Node q = p.next;
+            q.next = null;
+            q.data = null;
+        }
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        if(size == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public int length() {
+        return size;
+    }
+
+    public Object get(int index) {
+        if(index <0 || index >=size){
+            throw new IndexOutOfBoundsException("illegal index:"+index+">"+size);
+        }
+        Node p = head;
+        for(int i=0;i<=index;i++){
+            p = p.next;
+        }
+        return p.data;
+    }
+
+    public int indexOf(Object o) {
+        Node p = head.next;
+        for(int i = 0;i < size;i++){
+            if(p.data.equals(o)){
+                return i;
+            }
+            p = p.next;
+        }
+        return -1;
+    }
+
+    public Object pre(Object o) {
+        int index = indexOf(o);
+        if(index == -1 ||index == 0){
+            return null;
+        }else {
+            return get(index - 1);
+        }
+    }
+
+    public Object next(Object o) {
+        int index = indexOf(o);
+        if(index == -1 || index == 0 || index >= size-1){
+            return null;
+        }else{
+            return get(index+1);
+        }
+    }
+
+    public void insert(int index, Object o) {
+        Node p = head;
+        if(index < 0 || index > size){
+            throw new IndexOutOfBoundsException("illegal index:"+index+"{"+size+"}");
+        }
+        for(int i = 0; i<=index -1;i++){
+            p = p.next;
+        }
+
+        Node q = new Node(o, null);
+        q.next = p.next;
+        p.next = q;
+
+        size++;
+    }
+
+    public void delete(int index) {
+        Node p =head;
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("illegal index:"+index+"{"+size+"}");
+        }
+        for(int i = 0; i<=index -1;i++){
+            p = p.next;
+        }
+        Node back = p.next;
+        p.next = p.next.next;
+        size--;
+    }
+
+    public void display() {
+        int i = 0;
+        for(Node p = head.next;i<size;p=p.next,i++){
+            System.out.print(p.data+" ");
+        }
+        System.out.println();
+    }
+    public Node getHead(){
+        return head;
+    }
+
+    public void union(List lb) {
+    }
+
+    public List merge(List lb) {
+        return null;
+    }
+}
+
+class Node{
+     Object data;
+     Node next;
+     Node(Object data, Node next){
+         this.data = data;
+         this.next = next;
+     }
+
+}
+
+
--- list/src/test/java/ListTest.java
+++ list/src/test/java/ListTest.java
@@ -100,4 +100,95 @@ public class ListTest extends TestCase {
         ArrayList listMerge = list3.merge(list4);
         listMerge.display();
     }
+
+    public void testLinkedList(){
+        System.out.println("-------------线性链表初始化-------------");
+        List linkedList = new LinkedList();
+        linkedList.insert(0,1);
+        linkedList.insert(1,2);
+        linkedList.insert(0,3);
+        linkedList.insert(0,4);
+        linkedList.display();
+        System.out.println("线性链表大小："+linkedList.length());
+
+        System.out.println("-------获取元素-------");
+        System.out.println("第0号元素:"+linkedList.get(0));
+        System.out.println("值为1的元素的下标:"+linkedList.indexOf(1));
+        System.out.println("值为4的元素的下标:"+linkedList.indexOf(4));
+        System.out.println("值为1的元素前驱:"+linkedList.pre(1));
+        System.out.println("值为1的元素的后继:"+linkedList.next(1));
+        System.out.println("值为2的元素前驱:"+linkedList.pre(2));
+        System.out.println("值为2的元素的后继:"+linkedList.next(2));
+        System.out.println("值为3的元素前驱:"+linkedList.pre(3));
+        System.out.println("值为3的元素的后继:"+linkedList.next(3));
+
+        System.out.println("-------------线性链表删除-------------");
+        linkedList.delete(3);
+        linkedList.delete(0);
+        linkedList.display();
+        System.out.println("线性链表大小："+linkedList.length());
+
+        System.out.println("-------清空线性表-------");
+        linkedList.clear();
+        System.out.println("线性表长度:"+linkedList.length());
+        linkedList.display();
+
+        System.out.println("-------判空线性表-------");
+        System.out.println("线性表是否为空:"+linkedList.isEmpty());
+    }
+
+    public void testLinkedList1(){
+        System.out.println("-------初始化线性表-集合-------");
+        List list1 = new LinkedList();
+        list1.insert(0,5);
+        list1.insert(0,4);
+        list1.insert(0,3);
+        list1.insert(0,2);
+        list1.insert(0,1);
+        list1.insert(0,0);
+
+        List list2 = new LinkedList();
+        list2.insert(0,8);
+        list2.insert(0,7);
+        list2.insert(0,6);
+        list2.insert(0,5);
+        list2.insert(0,4);
+        list2.insert(0,3);
+
+        System.out.println("-------显示线性表-集合-------");
+
+        list1.display();
+        list2.display();
+
+        System.out.println("-------集合-------");
+        list1.union(list2);
+        list1.display();
+
+        System.out.println("-------初始化线性表-有序合并--------");
+        List list3 = new LinkedList();
+        list3.insert(0,5);
+        list3.insert(0,4);
+        list3.insert(0,3);
+        list3.insert(0,2);
+        list3.insert(0,1);
+        list3.insert(0,0);
+
+        List list4 = new LinkedList();
+        list4.insert(0,8);
+        list4.insert(0,7);
+        list4.insert(0,6);
+        list4.insert(0,5);
+        list4.insert(0,4);
+        list4.insert(0,3);
+
+        System.out.println("-------显示线性表-有序合并-------");
+
+        list3.display();
+        list2.display();
+
+        System.out.println("-------有序合并-------");
+        list3.merge(list4);
+        list3.display();
+    }
 }
+
