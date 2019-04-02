485346189f984bfbe6776957816fea79a7d689a9
coldJune <785691557@qq.com>
2019-04-02 20:19:08 +0800

    二分查找变形

--- Python/binarySearch/BinarySearch.py
+++ Python/binarySearch/BinarySearch.py
@@ -9,7 +9,7 @@ class BinarySearch:
 
     def binary_search(self, item):
         arr = self.__arr__
-        high = len(a)
+        high = len(arr) - 1
         low = 0
         while low <= high:
             mid = low + ((high-low) >> 2)
@@ -21,13 +21,79 @@ class BinarySearch:
                 return mid
         return -1
 
+    def first_equal(self, item):
+        arr = self.__arr__
+        high = len(arr) - 1
+        low = 0
+        while low <= high:
+            mid = low + ((high - low) >> 2)
+            if item < arr[mid]:
+                high = mid - 1
+            elif item > arr[mid]:
+                low = mid + 1
+            else:
+                if mid == 0 or arr[mid - 1] != item:
+                    return mid
+                high = mid - 1
+        return -1
+
+    def last_eq(self, item):
+        arr = self.__arr__
+        high = len(arr) - 1
+        low = 0
+        while low <= high:
+            mid = low + ((high - low) >> 2)
+            if item < arr[mid]:
+                high = mid - 1
+            elif item > arr[mid]:
+                low = mid + 1
+            else:
+                if mid == len(arr) - 1 or arr[mid + 1] != item:
+                    return mid
+                low = mid + 1
+        return -1
+
+    def last_lt(self, item):
+        arr = self.__arr__
+        high = len(arr) - 1
+        low = 0
+        while low <= high:
+            mid = low + ((high - low) >> 2)
+            if item <= arr[mid]:
+
+                high = mid - 1
+            else:
+                if mid == len(arr) - 1 or arr[mid + 1] > item:
+                    return mid
+                low = mid + 1
+        return -1
+
+    def first_gt(self, item):
+        arr = self.__arr__
+        high = len(arr) - 1
+        low = 0
+        while low <= high:
+            mid = low + ((high - low) >> 2)
+            if item <= arr[mid]:
+                if mid == 0 or arr[mid - 1] < item:
+                    return mid
+                high = mid - 1
+            else:
+
+                low = mid + 1
+        return -1
+
     def display(self):
         print(self.__arr__)
 
 
 if __name__ == '__main__':
-    a = [random.randint(1, 100) for i in range(10)]
+    a = [1, 2, 2, 4, 4, 4, 5]
     print(a)
     bs = BinarySearch(a)
     bs.display()
     print(bs.binary_search(a[2]))
+    print(bs.first_equal(2))
+    print(bs.last_eq(4))
+    print(bs.last_lt(3))
+    print(bs.first_gt(3))
